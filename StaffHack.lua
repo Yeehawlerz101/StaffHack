5f7b200cf8066a93ff5fca083f0ee8cde0bebbc8
Neil <yeehawlerz101@gmail.com>
2016-08-13 22:39:44 -0400

    (Broken_v0.0)
    Added Colors and Printed text when started up

---
+++ StaffHack.lua
@@ -0,0 +1,30 @@
+
+-- Colors --
+-- Desc: Just writing colors so instead of numbers I can just use terms like red or blue later on. --
+
+red = Color(255,0,0,255);
+black = Color(0,0,0,255);
+green = Color(0,255,0,255);
+white = Color(255,255,255,255);
+blue = Color(0,0,255,255);
+cyan = Color(0,255,255,255);
+pink = Color(255,0,255,255);
+blue = Color(0,0,255,255);
+grey = Color(100,100,100,255);
+gold = Color(255,228,0,255);
+lightblue = Color(155,205,248);
+lightgreen = Color(174,255,0);
+iceblue = Color(116,187,251,255);
+orange = Color(255,165,0,255);
+
+
+
+ MsgC( cyan,[[
+   _____ _         __  __ _                _    
+  / ____| |       / _|/ _| |              | |   
+ | (___ | |_ __ _| |_| |_| |__   __ _  ___| | __
+  \___ \| __/ _` |  _|  _| '_ \ / _` |/ __| |/ /
+  ____) | || (_| | | | | | | | | (_| | (__|   < 
+ |_____/ \__\__,_|_| |_| |_| |_|\__,_|\___|_|\_\
+                                                
+]])
